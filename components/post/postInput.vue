b531b627f46e7dcdeb766ab335a532b25bae654d
张亮 <2286377528@qq.com>
2020-02-20 19:50:27 +0800

    L-获取文章列表成功

--- components/post/postInput.vue
+++ components/post/postInput.vue
@@ -2,9 +2,12 @@
   <div class="input">
        <el-input
     placeholder="请输入想去的地方，比如：'广州'"
-    suffix-icon="el-icon-search"
-    v-model="input">
+    v-model="input"
+    @change='handleEnter'
+    >
      </el-input>
+     <i class="el-icon-search search" @click='handleClick'></i>
+     
 
     <span class="referr">推荐 &nbsp;
         <nuxt-link to='#'>广州</nuxt-link>
@@ -18,8 +21,44 @@
 export default {
   data () {
       return {
-          input : ''
+        // 输入的数据
+          input : '',
+          // 储存城市id
+          cityId : 1,
+          // 储存文章数据列表
+          textList : []
       }
+  },
+  mounted(){
+    if(!this.input) return;
+
+    this.$axios({
+      url : '/cities',
+      params : {
+        name :  this.input
+      }
+    }).then(res=>{
+      // console.log(res)
+      this.cityId = res.data.data[0].id;
+    })
+  },
+  methods: {
+    // 封装
+      init(){
+        this.$store.dispatch('post/getTextList',this.cityId).then(res =>{
+          //  console.log(res)
+          this.textList = res.data.data
+          this.$message.success('获取城市文章列表完成')
+         })
+      },
+    // 输入框change事件搜索文章列表
+       handleEnter(){
+          this.init();
+       },
+      //点击搜索键搜索
+       handleClick(){
+          this.init();
+       }
   }
 }
 </script>
@@ -44,5 +83,13 @@ export default {
         margin: 10px 0 0 0;
       color: #666;
       font-size: 12px;
+    }
+    .search {
+      position: absolute;
+      top: 10px;
+      right: 0;
+      color:orange;
+      font-size: 30px;
+      font-weight: 700;
     }
 </style>
