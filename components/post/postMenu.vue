342d06b62869fb928a69dc9da55dfe1129915b79
张亮 <2286377528@qq.com>
2020-02-20 13:41:46 +0800

    L-旅游攻略左侧静态完成

---
+++ components/post/postMenu.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="menu">
+     <div v-for="(item,index) in menuList" :key="index">
+      <p class="left">{{item.type}} <i class="el-icon-arrow-right"></i></p>
+      <!-- <div v-for='(value,index1) in item.children' :key="index1" class="show">
+         <p ><i>{{index1 + 1}}</i><span>{{value.city}}</span> {{value.desc}}</p>
+      </div> -->
+      <div class="show"></div>
+     </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+ data () {
+     return {
+        //  储存菜单数据
+        menuList : [],
+        // 控制弹框的显示与隐藏
+        current : false
+     }
+ },
+  mounted(){
+      this.$axios({
+          url : '/posts/cities'
+      }).then(res =>{
+        this.menuList = res.data.data
+        // console.log(this.menuList)
+      })
+  }
+}
+</script>
+
+<style scoped lang='less'>
+ .menu {
+     position: relative;
+     box-sizing: border-box;
+     border: 1px solid #ccc;
+     border-bottom: 0;
+    // width: 100%;
+     .left {
+       padding: 0 15px;
+       font-size: 15px;
+       height: 40px;
+       line-height: 40px;
+       border-bottom: 1px solid #ccc;
+       cursor: pointer;
+       &:hover {
+         color: orange;
+       }
+       >i {
+         margin-top: 10px;
+         float: right;
+     }
+     }
+     .show {
+     width: 300px;
+     height: 200px;
+     position: absolute;
+     top: 0;
+     right: -300px;
+     p {
+         height: 40px;
+     }
+     }
+ }
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,13 +1,69 @@
 <template>
-  <div>攻略首页</div>
+  <div class="main">
+    <el-row type="flex" class="row-bg" justify="space-between">
+      <!-- 左侧边导航栏 -->
+     <el-col :span="6"><div class="navs">
+       <!-- 上方菜单栏 -->
+        <PostMenu> </PostMenu>
+       <!-- 推荐城市 -->
+       <div class="goodCity">
+         <p>推荐城市</p>
+          <img src="http://157.122.54.189:9093/images/pic_sea.jpeg" alt="">
+       </div>
+    </div></el-col>
+      <!-- 右侧文章列表 -->
+   <el-col :span="17"><div class="article">
+       
+    </div></el-col>
+  
+    </el-row>
+
+
+
+  </div>
 </template>
 
 <script>
+import PostMenu from '@/components/post/postMenu'
 export default {
-
+  components: {
+   PostMenu
+  },
+  mounted(){
+   
+  }
 }
 </script>
 
-<style>
+<style scoped lang='less'>
+.main {
+  width: 1000px;
+  margin: 20px auto;
+  // 左侧布局
+  .navs {
+  //height: 500px;
+  width: 100%;
+  .goodCity {
+    margin-top: 20px;
+   p{
+     font-size: 15px;
+      height: 30px;
+     line-height: 30px;
+     border-bottom: 1px solid #ccc;
+     margin-bottom: 10px;
+     }
+    img { 
+      width: 100%;
+    }  
+  } 
+}
+// 右侧布局
+.article {
+  border: 1px solid black;
+  height: 500px;
+  width: 100%;
+}
+}
+
 
 </style>
